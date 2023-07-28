b8e90ab3d10baef509dcd8425894003bcc73beb1
Vusal Aliyev <vusalaliev02@gmail.com>
2023-07-28 13:18:23 +0400

    Add project files.

---
+++ LiquorStoreFinalProject/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using LiquorStoreFinalProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace LiquorStoreFinalProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
