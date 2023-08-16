2b86c5363a354e96cd354bd13303abd9d5c268cb
vusal.aliyev <vusal.aliyev@FCNT382>
2023-08-16 10:33:24 +0400

    HomePage works

--- LiquorStoreFinalProject/Controllers/HomeController.cs
+++ LiquorStoreFinalProject/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using LiquorStoreFinalProject.Models;
+﻿using LiquorStoreFinalProject.Data;
+using LiquorStoreFinalProject.Models;
+using LiquorStoreFinalProject.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -7,9 +9,26 @@ namespace LiquorStoreFinalProject.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly AppDbContext _context;
+
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> products = _context.Products;
+            IEnumerable<Category> categories = _context.Categories;
+            IEnumerable<Blog> blogs = _context.Blogs.Take(4).OrderByDescending(b=>b.CreatedDate);
+            HomeVM homeVM = new HomeVM()
+            {
+                Blogs = blogs,
+                Categories = categories,
+                Products = products
+            };
+
+            return View(homeVM);
         }
     }
 }
