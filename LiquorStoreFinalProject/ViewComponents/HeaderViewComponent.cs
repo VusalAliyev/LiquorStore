b7d02609c7f257b9b40fde5223c3d7049bfe5a1b
vusal.aliyev <vusal.aliyev@FCNT382>
2023-08-17 15:00:31 +0400

    Dynamic header

---
+++ LiquorStoreFinalProject/ViewComponents/HeaderViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using LiquorStoreFinalProject.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LiquorStoreFinalProject.ViewComponents
+{
+    public class HeaderViewComponent : ViewComponent
+    {
+        private readonly AppDbContext _context;
+
+        public HeaderViewComponent(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
